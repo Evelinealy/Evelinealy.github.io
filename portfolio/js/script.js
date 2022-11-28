e068a38d5a0116c207632aaa7581c32931fdfb2a
Aly Anderson <aly779372@gmail.com>
2022-11-27 17:28:43 -0800

    Add theme swapping and typing animation

---
+++ portfolio/js/script.js
@@ -0,0 +1,11 @@
+// ==== Typing Animation ====
+var typed = new Typed(".typing", {
+  strings: [
+    "Software Developer",
+    "Full-Stack Developer",
+    "Computer Science Major",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
